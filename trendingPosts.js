c263e1c4c53082431a83e61b191365cd2e953029
Edenkp <143792307+Edenkp@users.noreply.github.com>
2024-08-03 19:26:53 -1000

    Create trendingPosts.js

---
+++ trendingPosts.js
@@ -0,0 +1,149 @@
+//socialMedia.js data structure
+class User {
+  constructor(username, realName, age, gender, nicknames, workplace, location) {
+    this.username = username;
+    this.realName = realName;
+    this.age = age;
+    this.gender = gender;
+    this.nicknames = nicknames;
+    this.workplace = workplace;
+    this.location = location;
+    this.connections = [];  //connection objects
+    this.posts = [];  //post objects
+    this.readPosts = [];  //read post objects
+    this.comments = [];  //comment objects
+  }
+
+  addConnection(type, targetUser) {
+    this.connections.push(new Connection(type, targetUser));
+  }
+
+  addPost(content, creationTime) {
+    const post = new Post(content, creationTime, this);
+    this.posts.push(post);
+    return post;
+  }
+
+  addReadPost(post) {
+    this.readPosts.push(post);
+  }
+
+  addComment(content, creationTime, post) {
+    const comment = new Comment(content, creationTime, this);
+    this.comments.push(comment);
+    post.addComment(comment);
+  }
+}
+
+class Post {
+  constructor(content, creationTime, author) {
+    this.content = content;
+    this.creationTime = creationTime;
+    this.author = author;
+    this.comments = [];  // List of Comment objects
+    this.views = [];  // List of {user, viewTime}
+  }
+
+  addComment(comment) {
+    this.comments.push(comment);
+  }
+
+  addView(user, viewTime) {
+    this.views.push({ user, viewTime });
+  }
+}
+
+class Comment {
+  constructor(content, creationTime, author) {
+    this.content = content;
+    this.creationTime = creationTime;
+    this.author = author;
+  }
+}
+
+class Connection {
+  constructor(type, targetUser) {
+    this.type = type;
+    this.targetUser = targetUser;
+  }
+}
+
+//reportGenerator class
+class reportGenerator {
+  constructor(users) {
+    this.users = users; //list of User objects
+  }
+
+  //function that filters posts by keyword
+  filterPostsByKeyword(posts, keywords, include = true) {
+    return posts.filter(post => {
+      const containsKeyword = keywords.some(keyword => post.content.includes(keyword));
+      return include ? containsKeyword : !containsKeyword;
+    });
+  }
+
+  //function that filters posts by user attributes
+  filterPostsByUserAttributes(posts, attributes) {
+    return posts.filter(post => {
+      const user = post.author;
+      return Object.entries(attributes).every(([key, value]) => user[key] === value);
+    });
+  }
+
+  //function to generate the trending posts report
+  generateTrendingPostsReport(keywords = [], includeKeywords = true, userAttributes = {}) {
+    const allPosts = this.users.flatMap(user => user.posts);
+
+    //filter posts based on keywords
+    const filteredPosts = this.filterPostsByKeyword(allPosts, keywords, includeKeywords);
+
+    //filter posts based on user attributes
+    const finalPosts = this.filterPostsByUserAttributes(filteredPosts, userAttributes);
+
+    //sort posts by the number of views (trending rate)
+    const sortedPosts = finalPosts.sort((a, b) => b.views.length - a.views.length);
+
+    return sortedPosts;
+  }
+}
+
+//==========Testing==========//
+
+//create users
+const user1 = new User("alicinwonderland", "Alice", 25, "female", ["Ally"], "Apple", "Honolulu");
+const user2 = new User("bobthebuilder", "Bob", 30, "male", ["Bobby"], "Microsoft", "Tokyo");
+const user3 = new User("charliechaplin", "Charles", 35, "male", ["Charlie"], "Google", "Seoul");
+
+//add connections
+user1.addConnection("follows", user2);
+user2.addConnection("friends", user3);
+
+//add posts
+const post1 = user1.addPost("Hello, world! Check out this amazing post!", "2023-01-01T10:00:00Z");
+const post2 = user2.addPost("Learning JavaScript is so much fun!", "2023-01-02T11:00:00Z");
+const post3 = user3.addPost("Check out this cool JavaScript tutorial!", "2023-01-03T12:00:00Z");
+
+//add comments to posts
+user2.addComment("Nice post!", "2023-01-01T11:00:00Z", post1);
+user3.addComment("Agreed!", "2023-01-01T12:00:00Z", post1);
+
+//add views to posts
+post1.addView(user2, "2023-01-01T10:30:00Z");
+post1.addView(user3, "2023-01-01T11:30:00Z");
+post2.addView(user1, "2023-01-02T11:30:00Z");
+
+//create reportGenerator instance
+const reportGen = new reportGenerator([user1, user2, user3]);
+
+//generate report
+const trendingPosts = reportGen.generateTrendingPostsReport(
+  ["JavaScript"], //keywords to include
+  true,           //include posts containing the keywords
+  { age: 30 }      //user attributes to filter by
+);
+
+//display results
+console.log("Trending Posts:");
+trendingPosts.forEach(post => {
+  console.log(`Post by ${post.author.username}: ${post.content}`);
+});
